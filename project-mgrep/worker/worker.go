eb54274b6fa886c9af79f0a9f4e9667bfb196431
Medo <martinmederly@gmail.com>
2023-03-05 18:13:52 +0100

    mgrep project

---
+++ project-mgrep/worker/worker.go
@@ -0,0 +1,50 @@
+package worker
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+type Result struct {
+	Line    string
+	LineNum int
+	Path    string
+}
+
+type Results struct {
+	Inner []Result
+}
+
+func NewResult(line string, lineNum int, path string) Result {
+	return Result{line, lineNum, path}
+}
+
+func FindInFile(path string, find string) *Results {
+	file, err := os.Open(path)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return nil
+	}
+
+	results := Results{make([]Result, 0)}
+
+	scanner := bufio.NewScanner(file)
+	lineNum := 1
+
+	for scanner.Scan() {
+		text := scanner.Text()
+		if strings.Contains(text, find) {
+			r := NewResult(text, lineNum, path)
+			results.Inner = append(results.Inner, r)
+		}
+		lineNum++
+	}
+
+	if len(results.Inner) == 0 {
+		return nil
+	} else {
+		return &results
+	}
+}
